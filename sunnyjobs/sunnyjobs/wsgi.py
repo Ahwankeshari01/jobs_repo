f571b2c95b7b27e02f0d54087b2e4386750c4f37
Ahwankeshari01 <ahwankesharij@gmail.com>
2025-10-29 14:44:11 +0530

    Configure WSGI path for Django application
    Added path configuration for WSGI application.

--- sunnyjobs/sunnyjobs/wsgi.py
+++ sunnyjobs/sunnyjobs/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+path = '/home/maheshproject/jobs_repo/sunnyjobs'
+if path not in sys.path:
+    sys.path.append(path)
+    os.chdir(path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','sunnyjobs.settings')
+import django
+django.setup()
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sunnyjobs.settings')
 
 application = get_wsgi_application()
+https://github.com/Ahwankeshari01/jobs_repo.git
